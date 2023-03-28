65aca6f5fd2a9e361ec50ebf5d00db911f3445a4
Thomas Werthenbach <t.a.k.werthenbach@student.tudelft.nl>
2023-03-28 16:13:29 +0200

    added backdoor attack for MNIST

--- experiment_infrastructure/decentralized_learning/base_node.py
+++ experiment_infrastructure/decentralized_learning/base_node.py
@@ -62,9 +62,9 @@ class BaseNode(ABC):
             model.parameters(),
             lr=settings.learning_rate,
             momentum=settings.momentum)
+        error = nn.CrossEntropyLoss()
 
         model.train()
-        train_loss = 0
         self.logger.info(f"Training for {settings.epochs} epochs with dataset length: {len(dataset)}")
         for _ in range(settings.epochs):
             for i, data in enumerate(dataset):
@@ -72,7 +72,6 @@ class BaseNode(ABC):
                 inputs, labels = inputs.to(device), labels.to(device)
                 optimizer.zero_grad()
                 outputs = model(inputs)
-                loss = F.nll_loss(outputs, labels)
-                train_loss += F.nll_loss(outputs, labels, reduction='sum').item()
+                loss = error(outputs, labels)
                 loss.backward()
                 optimizer.step()

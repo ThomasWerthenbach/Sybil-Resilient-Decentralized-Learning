194e00fa9b6b03bbc124958d5e54037ff8c578cc
Thomas Werthenbach <t.a.k.werthenbach@student.tudelft.nl>
2023-04-02 14:50:15 +0200

    Backdoor dataset script

--- scripts/count_amount_of_trainable_parameters.py
+++ scripts/count_amount_of_trainable_parameters.py
@@ -1,62 +1,7 @@
 import numpy as np
-from torch import nn
 
-from ml.models.CIFAR10CNN import ResNet8, ResNet14, ResNet56, ResNet44, ResNet20, ResNet26, ResNet32
 from ml.models.CIFAR10LENET import LeNet
-from ml.models.FasterMNIST import MNIST
-from ml.models.model import Model
-import torch.nn.functional as F
-import torch.optim as optim
-
-from ml.util import serialize_model
-import torch
-import torch.nn.functional as F
-from torch import nn
-
-class CNN(nn.Module):
-    """
-    Class for a CNN Model for Celeba
-
-    """
-
-    def __init__(self):
-        """
-        Constructor. Instantiates the CNN Model
-            with 84*84*3 Input and 2 output classes
-
-        """
-        super().__init__()
-        # 2.8k parameters
-        self.conv1 = nn.Conv2d(3, 32, 3, padding="same")
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(32, 32, 3, padding="same")
-        self.conv3 = nn.Conv2d(32, 32, 3, padding="same")
-        self.conv4 = nn.Conv2d(32, 32, 3, padding="same")
-        self.fc1 = nn.Linear(5 * 5 * 32, 10)
-
-    def forward(self, x):
-        """
-        Forward pass of the model
-
-        Parameters
-        ----------
-        x : torch.tensor
-            The input torch tensor
-
-        Returns
-        -------
-        torch.tensor
-            The output torch tensor
-
-        """
-        x = F.relu(self.pool(self.conv1(x)))
-        x = F.relu(self.pool(self.conv2(x)))
-        x = F.relu(self.pool(self.conv3(x)))
-        x = F.relu(self.pool(self.conv4(x)))
-        x = torch.flatten(x, 1)
-        x = self.fc1(x)
-        return x
-
+from ml.models.Celeba import CNN
 
 if __name__ == '__main__':
 
--- scripts/sybil_dataset.py
+++ scripts/sybil_dataset.py
@@ -1,10 +1,28 @@
+import os
+
+import torchvision
+from torch.utils.data import DataLoader
+from torchvision.transforms import ToTensor
+
+from experiment_infrastructure.attacks.backdoor import Backdoor
 from experiment_infrastructure.attacks.label_flip import LabelFlip
+from experiment_infrastructure.experiment_settings.settings import Settings
 from ml.datasets.MNIST import MNISTDataset
 
 if __name__ == "__main__":
-    a = MNISTDataset().get_peer_dataset(1, 100, sybil_data_transformer=LabelFlip(0, 1))
-    b = MNISTDataset().get_peer_dataset(1, 100)
-    al = list(a)
-    bl = list(b)
-    for first, second in zip(al, bl):
-        print(first[1], "\t", second[1])
+    # d = DataLoader(torchvision.datasets.MNIST(
+    #     root=os.path.join(os.path.dirname(__file__), '../data', 'train'), train=True, download=True,
+    #     transform=ToTensor()
+    # ), batch_size=1)
+
+    filename = os.path.join(os.path.dirname(__file__), 'settings.json')
+    with open(filename) as f:
+        s = Settings.from_json("".join([x.strip() for x in f.readlines()]))
+
+    d = MNISTDataset().get_peer_dataset(0, s.peers_per_host * s.total_hosts, sybil_data_transformer=Backdoor(s))
+
+    for data, target in d:
+        print(data[0][0])
+        # print(data.shape)
+        print(target)
+        break
